4142ec0cd724a887845a9e2503ac230c81e868f8
SIDDHARTHA DEB <siddhartha_deb92@rediffmail.com>
2022-07-23 01:32:56 +0530

    This is my first code push in Git

---
+++ mongotest.py
@@ -0,0 +1,18 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://siddharthadeb:9755408244@cluster-sid01.ylcne.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d = {
+    "name" : "siddhartha",
+    "email":"siddharthadeb92@gmil.com",
+    "surname" : "deb"
+}
+
+db1 = client['mongotest']
+#collection
+coll = db1['test']
+#insert dictionary
+coll.insert_one(d)
